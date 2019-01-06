ccc6748108442fa8b300d3728861af60e1de9cab
qyb225 <qyb225@gmail.com>
2019-01-06 15:52:26 +0800

    [api] user register, info searching api

---
+++ controller/user_controller.go
@@ -0,0 +1,74 @@
+package controller
+
+import (
+	"midgo/httpsvr"
+
+	"midnightapisvr/service"
+)
+
+// UserController 定义用户相关信息的 api
+type UserController struct{}
+
+// UserRegister 用户注册
+func (*UserController) UserRegister(c *httpsvr.Req) *httpsvr.Resp {
+	c.ParseForm()
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	nickname := c.FormValue("nickname")
+
+	var ret = new(httpsvr.Resp)
+	if len(username) < 6 || len(password) < 6 || len(nickname) == 0 {
+		ret.SetRtn(400)
+		ret.SetMsg("param error, check if username, password length < 6")
+		return ret
+	}
+
+	var userSvc = new(service.UserService)
+	const AVATAR_URI = "/midnightstatic/images/avatar_default.jpg"
+	userInfo, err := userSvc.UserRegister(username, nickname, password, AVATAR_URI)
+	if err != nil {
+		ret.SetRtn(500)
+		ret.SetMsg(err.Error())
+		return ret
+	}
+
+	ret.SetRtn(0)
+	ret.SetMsg("okay")
+	ret.SetData(userInfo)
+	return ret
+}
+
+// GetUserInfo 获取用户信息
+func (*UserController) GetUserInfo(c *httpsvr.Req) *httpsvr.Resp {
+	c.ParseForm()
+	token := c.FormValue("token")
+
+	var ret = new(httpsvr.Resp)
+	if len(token) == 0 {
+		ret.SetRtn(403)
+		ret.SetMsg("permission denied: need token")
+		return ret
+	}
+
+	var sessionSvc = new(service.SessionService)
+	uid, err := sessionSvc.GetUidByToken(token)
+	if err != nil || uid <= 0 {
+		ret.SetRtn(403)
+		ret.SetMsg("permission denied: invalid token")
+		return ret
+	}
+
+	var userSvc = new(service.UserService)
+	userInfo, err := userSvc.GetUserInfo(uid)
+	if err != nil {
+		ret.SetRtn(500)
+		ret.SetMsg(err.Error())
+		return ret
+	}
+
+	ret.SetRtn(0)
+	ret.SetMsg("okay")
+	ret.SetData(userInfo)
+
+	return ret
+}
