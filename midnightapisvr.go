8d7c8d3c2848307d1686ce1d85be8375b4173ca9
qyb225 <qyb225@gmail.com>
2019-01-06 15:53:43 +0800

    [api] add controller to svr handler

--- midnightapisvr.go
+++ midnightapisvr.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"midgo/httpsvr"
+
+	"midnightapisvr/controller"
 )
 
 const (
@@ -11,5 +13,8 @@ const (
 
 func main() {
 	svr := httpsvr.GetMidgoSvr()
+	svr.AddController("/midnightapisvr/api", new(controller.UserController))
+	svr.AddController("/midnightapisvr/api", new(controller.SessionController))
+	svr.AddController("/midnightapisvr/api", new(controller.ActionController))
 	svr.Run(IPADDR, PORT)
 }
