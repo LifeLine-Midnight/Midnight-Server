082c4bb09d2d690cfaf7b780b294ce7864cfa782
qyb225 <qyb225@gmail.com>
2019-01-06 15:48:05 +0800

    [comm] global struct define

---
+++ comm/global_struct.go
@@ -0,0 +1,17 @@
+package comm
+
+// 对应 db 的 story.conjunction_msg_type
+const (
+	MSG_NONE    = 0
+	MSG_TEXT    = 1
+	MSG_CHOOSE  = 2
+	MSG_NEWS    = 3
+	MSG_MOMENT  = 4
+	MSG_ONLINE  = 5
+	MSG_OFFLINE = 6
+)
+
+const (
+	LCHOOSE = 0
+	RCHOOSE = 1
+)
